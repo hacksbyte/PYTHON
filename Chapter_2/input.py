3396c3abc255ebb2af7ccf751ce9aeb574313312
hacksbyte <76638715+hacksbyte@users.noreply.github.com>
2021-02-18 00:35:11 -0800

    Update input.py

--- Chapter_2/input.py
+++ Chapter_2/input.py
@@ -2,3 +2,9 @@ a = input("enter your number : ")
 b = input("enter your Name : ")
 print (type(a))
 print(type(b))
+
+c = int(input("apple 65 rupees/KG how much you want : "))
+# d = int(input(65))
+c *= 65
+print ("aplle :" + str(c))
+print (type(c))
