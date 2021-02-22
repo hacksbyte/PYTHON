6b62af90d7fb5693267a8f6cbca3c6279a22708c
hacksbyte <76638715+hacksbyte@users.noreply.github.com>
2021-02-22 07:30:59 -0800

    Delete stringfunction.py

--- Chapter_3/stringfunction.py
+++
@@ -1,9 +0,0 @@
-story = "once upon a time there was a youtuber named Harry who uploaded python course with notes Harry"
-
-# String Functions
-print(len(story))
-print (story.count("t"))
-print (story.find("once"))
-print (story.capitalize())
-print (story.endswith("notes"))
-print (story.replace("youtuber", "youtube"))
