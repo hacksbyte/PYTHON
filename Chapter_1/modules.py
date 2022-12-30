26e641b95597544ae8df2e39868808540e54306f
hacksbyte <76638715+hacksbyte@users.noreply.github.com>
2022-12-30 07:22:42 +0530

    Update modules.py

--- Chapter_1/modules.py
+++ Chapter_1/modules.py
@@ -1,4 +1,4 @@
 # Modules is a file containing code written by somebody else (usually) which can be imported and used in our program
 # this is our Module like playsound, pyinstaller os
 from playsound import playsound
-playsound('On and On(PaglaSongs).mp3')
+playsound('your music name')
